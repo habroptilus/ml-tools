b88f2211ebab29228d8180ef4d1c30caa7258953
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2019-05-21 15:39:47 +0900

    動くとこまで

--- tools/models/classifiers/linear_subgrad_svm.py
+++ tools/models/classifiers/linear_subgrad_svm.py
@@ -1,8 +1,9 @@
 import numpy as np
 from sklearn.utils.validation import check_is_fitted
+from sklearn.metrics import accuracy_score
 
 
-class LinearSVMsubgrad():
+class LinearSubgradSVM():
     """線形モデルにサポートベクターマシンの劣勾配アルゴリズムを適用したモデル."""
 
     def __init__(self, lam, lr, max_iter, epsilon):
@@ -24,12 +25,12 @@ class LinearSVMsubgrad():
         for i in range(self.max_iter):
             theta_next = theta - self.lr * self.grad(phi, y, theta, K)
             if np.linalg.norm(theta_next - theta) < self.epsilon:
-                print("Converged!")
+                print(f"Converged! loss {self.loss(phi,y,theta,K)}")
                 theta = theta_next
                 break
             theta = theta_next
-            if (i + 1) % 100 == 0:
-                print(f"iter {i+1}")
+            if (i + 1) % 1000 == 0:
+                print(f"iter {i+1} loss {self.loss(phi,y,theta,K)}")
         self.coef_ = theta
 
     def grad(self, phi, y, theta, K):
@@ -37,13 +38,21 @@ class LinearSVMsubgrad():
 
     def subgrad(self, phi, y, theta):
         scores = 1 - np.dot(phi, theta) * y
-        s_grad = -sum((phi * y)[scores > 0])
+        y = y.reshape((len(y), 1))
+        if sum(scores) > 0:
+            s_grad = -sum((y * phi)[scores > 0])
+        else:
+            s_grad = np.zeros(len(theta))
         assert len(s_grad) == len(theta), s_grad.shape
         return s_grad
 
+    def loss(self, phi, y, theta, K):
+        return sum(np.maximum(0, 1 - np.dot(phi, theta) * y)) + \
+            self.lam * np.dot(np.dot(theta.T, K), theta) / 2
+
     def get_K(self, phi):
         """calc products for K."""
-        return np.dot(phi, phi.T)
+        return np.dot(phi.T, phi)
 
     def init_theta(self, features_n):
         """initialize theta.
@@ -58,5 +67,9 @@ class LinearSVMsubgrad():
 
     def add_const(self, X):
         """add const to X"""
-        const = np.ones(len(X))
-        return np.stack([const, X], axis=1)
+        const = np.ones((len(X), 1))
+        return np.hstack((const, X))
+
+    def evaluate(self, X, y):
+        pred_y = self.predict(X)
+        return accuracy_score(y, pred_y)
