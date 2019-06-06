77852f4ac8a5682bc799dd20768023ab2294397e
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2019-06-06 22:59:42 +0900

    いちおうできた

--- tools/models/regressors/gaussian_process_regressor.py
+++ tools/models/regressors/gaussian_process_regressor.py
@@ -2,29 +2,35 @@ import numpy as np
 
 
 class GaussianProcessRegressor:
-    def __init__(self, kernel):
+    def __init__(self, kernel, c):
         self.kernel = kernel
+        self.c = c
 
     def fit(self, X, y):
         self.X_train = X
-        self.y_train = y
-        self.K = self.get_gram_matrix(X, X)
+        self.y_mean = np.mean(y)
+        y_train = y - self.y_mean
+        self.y_train = np.reshape(y_train, (len(y_train), 1))
+        self.K = self.get_gram_matrix(X, X, flag=True)
 
     def predict(self, X):
         mu, _ = self.calc_mu_sigma(X)
         return mu
 
     def calc_mu_sigma(self, X):
-        y = np.reshape(self.y_train, (len(self.y_train), 1))
-        k_star = self.get_gram_matrix(self.X_train, X)
-        k_starstar = self.kernel.run(X, X)
+        k_star = self.get_gram_matrix(self.X_train, X, flag=False)
+        k_starstar = self.kernel.run(X, X) + self.c
         K_inv = np.linalg.inv(self.K)
-        mu = k_star.T @ K_inv @ y
+        mu = k_star.T @ K_inv @ self.y_train
         sigma = k_starstar - k_star.T@K_inv@k_star
-        return mu, sigma
+        return mu + self.y_mean, sigma
 
-    def get_gram_matrix(self, A, B):
+    def get_gram_matrix(self, A, B, flag):
         """AとBのgram行列を返す.
         return's shape (len(A),len(B))
+        flag: クロネッカーつけるかどうか
         """
-        return np.array([[self.kernel.run(A[i], B[j]) for j in range(len(B))] for i in range(len(A))])
+        return np.array([[self.kernel.run(A[i], B[j]) + self.c * self.kronecker(i, j, flag) for j in range(len(B))] for i in range(len(A))])
+
+    def kronecker(self, i, j, flag):
+        return 1 if i == j and flag else 0
